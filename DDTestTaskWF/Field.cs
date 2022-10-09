1c63faa0264c94b555be75a7e8c96cb417c75c1c
KirillZhel <Zheltyshev.kirill@gmail.com>
2022-10-09 18:54:19 +0300

    fixes

--- DDTestTaskWF/Field.cs
+++ DDTestTaskWF/Field.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace DDTestTaskWF
+﻿namespace DDTestTaskWF
 {
 	public partial class Field : Form
 	{
